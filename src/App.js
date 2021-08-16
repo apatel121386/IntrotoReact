d6993867a124a503ea7723502ccb391e43af1417
apatel121386 <apatel121386@outlook.com>
2021-08-16 16:14:50 -0500

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Home from './views/Home';
+import About from './views/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('Constructor Method Ran')
+    this.state = {
+      myName: 'Ankit'
+    }
+  }
+  updateName=()=> {
+    const newName = prompt('What is your name?')
+    this.setState({
+      myName: newName
+    })
+  }
+
+  componentDidMount(){
+    console.log('Component Did Mount...')
+    this.setState(
+      {
+        myName: 'Ankit'
+      }
+    )
+  }
+
+  render() {
+    console.log('Render Method Ran')
+    const myName = this.state.myName;
+    return (
+      <div>
+        <Navbar myName={myName} />
+        <div className="container">
+          <Switch>
+            <Route exact path='/'>
+              <Home myName={myName} updateName={this.updateName}/>
+          </Route>
+          <Route path='/about'>
+            <About/>
+           </Route>
+           </Switch>
+        </div>
+        
+      </div>
+    )
+  }
 }
-
-export default App;
