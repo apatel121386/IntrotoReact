33bff9aa25f82d784e70befd9a23bcfbdbf226db
apatel121386 <apatel121386@outlook.com>
2021-08-18 11:52:19 -0500

    Monday_update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Switch, Route} from 'react-router-dom';
-import Navbar from './components/Navbar'
-import Home from './views/Home';
+import Navbar from './components/Navbar';
 import About from './views/About';
+import Home from './views/Home';
+import Users from './views/Users';
 
 export default class App extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
-    console.log('Constructor Method Ran')
+    console.log('Component Constructing...')
     this.state = {
-      myName: 'Ankit'
+      myName: 'Ankit',
+      racers: []
     }
   }
-  updateName=()=> {
+
+  updateName = () => {
     const newName = prompt('What is your name?')
     this.setState({
       myName: newName
@@ -21,30 +24,35 @@ export default class App extends Component {
 
   componentDidMount(){
     console.log('Component Did Mount...')
-    this.setState(
-      {
-        myName: 'Ankit'
-      }
-    )
+    fetch('https://ergast.com/api/f1/2021/10/driverStandings.json')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          racers: data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+        })
+      })
   }
 
   render() {
-    console.log('Render Method Ran')
+    console.log('Component Rendering...')
     const myName = this.state.myName;
     return (
       <div>
-        <Navbar myName={myName} />
-        <div className="container">
+        <Navbar myName={myName}/>
+        <div className='container'>
           <Switch>
             <Route exact path='/'>
-              <Home myName={myName} updateName={this.updateName}/>
-          </Route>
-          <Route path='/about'>
-            <About/>
-           </Route>
-           </Switch>
+              <Home myName={myName} updateName={this.updateName} allRacers={this.state.racers}/>
+            </Route>
+            <Route exact path='/about'>
+              <About />
+            </Route>
+            <Route exact path='/users'>
+              <Users />
+            </Route>
+          </Switch>
         </div>
-        
       </div>
     )
   }
